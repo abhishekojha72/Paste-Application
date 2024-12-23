8e8bac986ca5061366cb87dddbfc492de02fccb2
abhishekojha72 <ojhah96@gmail.com>
2024-12-23 18:02:37 +0530

    Add Tailwind Css
    Add Tailwind Css

--- PasteApplication/tailwind.config.js
+++ PasteApplication/tailwind.config.js
@@ -1,9 +1,11 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+module.exports = {
+  content: [
+    './src/**/*.{html,js,jsx,ts,tsx}', // Adjust this to the paths where your HTML/JSX/TSX files are located
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
+
 
